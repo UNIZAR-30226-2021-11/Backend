c309a7b62860cc73d83663cfc2561d1e00f34e84
javierherrer <72160400+javierherrer@users.noreply.github.com>
2021-03-23 10:01:23 +0100

    models.sql Updated

--- database/models.sql
+++ database/models.sql
@@ -3,43 +3,36 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(150) NOT NULL UNIQUE,
     password varchar(256) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE,
-    year INT NOT NULL,
-    admin BOOLEAN NOT NULL,
-    picture VARCHAR(256) DEFAULT 'https://placekitten.com/g/300/300',
+    location varchar(150),
+    games_won INT DEFAULT 0,
+    games_lost INT DEFAULT 0,
     created_at timestamp DEFAULT now(),
     updated_at timestamp NOT NULL,
     CONSTRAINT pk_users PRIMARY KEY(id)
 );
 
-CREATE TABLE IF NOT EXISTS subjects (
+CREATE TABLE IF NOT EXISTS games (
     id serial NOT NULL,
     name VARCHAR(150) NOT NULL,
-    year INT NOT NULL,
-    CONSTRAINT pk_subjects PRIMARY KEY(id)
+    public BOOLEAN NOT NULL,
+    creation_date timestamp DEFAULT now(),
+    end_date timestamp,
+    CONSTRAINT pk_games PRIMARY KEY(id)
 );
 
-CREATE TABLE IF NOT EXISTS posts (
+CREATE TABLE IF NOT EXISTS pairs (
     id serial NOT NULL,
-    user_id int NOT NULL,
-    subject_id int NOT NULL,
-    title VARCHAR(150) NOT NULL,
-    category VARCHAR(150) NOT NULL,
-    body text NOT NULL,
-    created_at timestamp DEFAULT now(),
-    updated_at timestamp NOT NULL,
-    CONSTRAINT pk_posts PRIMARY KEY(id),
-    CONSTRAINT fk_posts_users FOREIGN KEY(user_id) REFERENCES users(id),
-    CONSTRAINT fk_posts_subjects FOREIGN KEY(subject_id) REFERENCES subjects(id)
+    game_points INT DEFAULT 0,
+    game_id INT NOT NULL,
+    CONSTRAINT pk_pairs PRIMARY KEY(id),
+    CONSTRAINT fk_pairs_games FOREIGN KEY(game_id) REFERENCES games(id)
 );
 
-CREATE TABLE IF NOT EXISTS replies (
+CREATE TABLE IF NOT EXISTS players (
     id serial NOT NULL,
-    user_id int NOT NULL,
-    post_id int NOT NULL,
-    body text NOT NULL,
-    created_at timestamp DEFAULT now(),
-    updated_at timestamp NOT NULL,
-    CONSTRAINT pk_replies PRIMARY KEY(id),
-    CONSTRAINT fk_replies_users FOREIGN KEY(user_id) REFERENCES users(id),
-    CONSTRAINT fk_replies_posts FOREIGN KEY(post_id) REFERENCES posts(id)
+    user_id INT NOT NULL,
+    pair_id INT NOT NULL,
+    CONSTRAINT pk_players PRIMARY KEY(id),
+    CONSTRAINT fk_players_users FOREIGN KEY(user_id) REFERENCES users(id),
+    CONSTRAINT fk_players_pairs FOREIGN KEY(pair_id) REFERENCES pairs(id)
 );
---
+++ go.mod
@@ -0,0 +1,10 @@
+module Backend
+
+go 1.15
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-chi/chi v1.5.4
+	github.com/lib/pq v1.10.0
+	golang.org/x/crypto v0.0.0-20210317152858-513c2a44f670
+)
