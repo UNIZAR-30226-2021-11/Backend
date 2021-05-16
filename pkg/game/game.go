b8cf8bb4fb12352efaff7645137493de541a2eb1
javierherrer <72160400+javierherrer@users.noreply.github.com>
2021-05-16 12:47:12 +0200

    Revert "EndGame"
    This reverts commit a7e8fc65911e1822bec822d2964003a8cc43aa1c.

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -7,7 +7,6 @@ import (
 
 // Game created by a user.
 type Game struct {
-<<<<<<< Updated upstream
 	ID           uint        `json:"id,omitempty"`
 	MyPairID     uint        `json:"my_pair_id,omitempty"`
 	MyPlayerID   uint        `json:"my_player_id,omitempty"`
@@ -21,18 +20,3 @@ type Game struct {
 	CreationDate time.Time   `json:"creation_date,omitempty"`
 	EndDate      time.Time   `json:"end_date,omitempty"`
 }
-=======
-	ID     		 uint      	`json:"id,omitempty"`
-	MyPairID	 uint		`json:"my_pair_id,omitempty"`
-	MyPlayerID	 uint		`json:"my_player_id,omitempty"`
-	Name 		 string		`json:"name,omitempty"`
-	Public   	 bool		`json:"public,omitempty"`
-	PlayersCount int 		`json:"players_count,omitempty"`
-	Winned 		 bool		`json:"winned,omitempty"`
-	WinnedPair	 uint		`json:"winned_pair,omitempty"`
-	Points		 int 		`json:"points,omitempty"`
-	Pairs		 []pair.Pair`json:"pairs,omitempty"`
-	CreationDate time.Time 	`json:"creation_date,omitempty"`
-	EndDate		 time.Time 	`json:"end_date,omitempty"`
-}
->>>>>>> Stashed changes
